7db062feb164fb630cd9e2fc459d70786d762009
notforgiving <notforgiving@list.ru>
2021-01-20 16:44:01 +0300

    Третий день

--- #src/js/script.js
+++ #src/js/script.js
@@ -4,40 +4,36 @@ const visableMenu = document.querySelector(".header__social");
 const menuBtn = document.querySelector("#menu-btn");
 const mobileMenu = document.querySelector("#menu_checkbox");
 
-try{
+try {
   menuBtn.addEventListener("click", () => {
     headerMenu.classList.toggle("showFlex");
     visableMenu.classList.toggle("lowMargin");
     menuBtn.classList.toggle("yellowColor");
   });
-}
-catch{
-}
+} catch {}
 
-try{
-  mobileMenu.addEventListener('click',()=>{
+try {
+  mobileMenu.addEventListener("click", () => {
     headerMenu.classList.toggle("showFlex");
     visableMenu.classList.toggle("lowMargin");
     menuBtn.classList.toggle("yellowColor");
-  })
+  });
+} catch {}
+
+if (window.location.pathname == "/") {
+  /*Слайдер - баннер*/
+  $(".epic-slider").slick({
+    dots: false,
+    arrows: false,
+  });
+
+  // /*Слайдер - популярное*/
+
+  $(".popular-category__slider").slick({
+    dots: false,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+  });
 }
-catch{
-}
-
-
-/*Слайдер - баннер*/
-
-// $(".epic-slider").slick({
-//   dots: false,
-//   arrows: false,
-// });
-
-// /*Слайдер - популярное*/
-
-// $(".popular-category__slider").slick({
-//   dots: false,
-//   arrows: false,
-//   slidesToShow: 3,
-//   slidesToScroll: 1,
-//   variableWidth: true,
-// });
