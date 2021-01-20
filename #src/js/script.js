bef27678d0fbc346e0a916700792ca9eaa15897d
notforgiving <notforgiving@list.ru>
2021-01-20 16:02:37 +0300

    Третий день
    9.38-13.10
    13.49-16.02

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,16 +1,43 @@
 /* Раскрытие меню */
-const menuBtn = document.querySelector('#menu-btn')
-const headerMenu = document.querySelector('.header__menu')
-const visableMenu = document.querySelector('.header__social')
-menuBtn.addEventListener('click',()=>{
-  headerMenu.classList.toggle('showFlex')
-  visableMenu.classList.toggle('lowMargin')
-  menuBtn.classList.toggle('yellowColor')
-})
+const headerMenu = document.querySelector(".header__menu");
+const visableMenu = document.querySelector(".header__social");
+const menuBtn = document.querySelector("#menu-btn");
+const mobileMenu = document.querySelector("#menu_checkbox");
 
-/*Слайдер */
+try{
+  menuBtn.addEventListener("click", () => {
+    headerMenu.classList.toggle("showFlex");
+    visableMenu.classList.toggle("lowMargin");
+    menuBtn.classList.toggle("yellowColor");
+  });
+}
+catch{
+}
 
-$('.epic-slider').slick({
-  dots: false,
-  arrows:false
-});
+try{
+  mobileMenu.addEventListener('click',()=>{
+    headerMenu.classList.toggle("showFlex");
+    visableMenu.classList.toggle("lowMargin");
+    menuBtn.classList.toggle("yellowColor");
+  })
+}
+catch{
+}
+
+
+/*Слайдер - баннер*/
+
+// $(".epic-slider").slick({
+//   dots: false,
+//   arrows: false,
+// });
+
+// /*Слайдер - популярное*/
+
+// $(".popular-category__slider").slick({
+//   dots: false,
+//   arrows: false,
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   variableWidth: true,
+// });
