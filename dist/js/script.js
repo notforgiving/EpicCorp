71ce4227cb5fbece686302fb8668e7c7af6811f5
notforgiving <notforgiving@list.ru>
2021-01-22 20:25:50 +0300

    четвертый день
    11.30-13.30
    14.00-15.39
    17.30-20.25

--- dist/js/script.js
+++ dist/js/script.js
@@ -27,13 +27,41 @@ if (window.location.pathname == "/") {
     arrows: false,
   });
 
-  // /*Слайдер - популярное*/
-
+  /*Слайдер - популярное*/
   $(".popular-category__slider").slick({
     dots: false,
     arrows: false,
     slidesToShow: 3,
     slidesToScroll: 1,
     variableWidth: true,
+  });
+} else if (window.location.pathname == "/card.html") {
+  /*Слайдер - карточка товара*/
+  $(".card-item__images-slider").slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+    prevArrow:
+      '<div class="images-slider__arrows"><svg class="vartical-arrow-top" xmlns="http://www.w3.org/2000/svg" width="16" height="10" viewBox="0 0 16 10"><g><g><path fill="#787677" d="M.307 8.582L7.088.294a.892.892 0 0 1 .763-.325c.281-.022.57.09.762.325l6.781 8.288a.893.893 0 0 1-1.381 1.13L7.85 2.182 1.688 9.713a.892.892 0 0 1-1.381-1.13z"/></g></g></svg></div>',
+    nextArrow:
+      '<div class="images-slider__arrows"><svg class="vartical-arrow-bottom" xmlns="http://www.w3.org/2000/svg" width="16" height="10" viewBox="0 0 16 10"><g><g><path fill="#787677" d="M.307 8.582L7.088.294a.892.892 0 0 1 .763-.325c.281-.022.57.09.762.325l6.781 8.288a.893.893 0 0 1-1.381 1.13L7.85 2.182 1.688 9.713a.892.892 0 0 1-1.381-1.13z"/></g></g></svg></div>',
+  });
+  /* Слайдер - рекомендации*/
+  $(".card-item__recomendation-slider").slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToScroll: 1,
+    prevArrow:
+      '<div class="recomendation-slider__arrows"><svg class="recomendation-slider__arrow-left" xmlns="http://www.w3.org/2000/svg" width="16" height="10" viewBox="0 0 16 10"><g><g><path fill="#787677" d="M.307 8.582L7.088.294a.892.892 0 0 1 .763-.325c.281-.022.57.09.762.325l6.781 8.288a.893.893 0 0 1-1.381 1.13L7.85 2.182 1.688 9.713a.892.892 0 0 1-1.381-1.13z"/></g></g></svg></div>',
+    nextArrow:
+      '<div class="recomendation-slider__arrows"><svg class="recomendation-slider__arrow-right" xmlns="http://www.w3.org/2000/svg" width="16" height="10" viewBox="0 0 16 10"><g><g><path fill="#787677" d="M.307 8.582L7.088.294a.892.892 0 0 1 .763-.325c.281-.022.57.09.762.325l6.781 8.288a.893.893 0 0 1-1.381 1.13L7.85 2.182 1.688 9.713a.892.892 0 0 1-1.381-1.13z"/></g></g></svg></div>',
   });
 }
