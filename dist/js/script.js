b0a1a8d0f5c2fc558b113ca5957dab570f389cd9
notforgiving <notforgiving@list.ru>
2021-01-25 13:00:00 +0300

    День шестой
    10.25-12.59

--- dist/js/script.js
+++ dist/js/script.js
@@ -69,5 +69,70 @@ if (window.location.pathname == "/") {
       '<div class="recomendation-slider__arrows"><svg class="recomendation-slider__arrow-right" xmlns="http://www.w3.org/2000/svg" width="16" height="10" viewBox="0 0 16 10"><g><g><path fill="#787677" d="M.307 8.582L7.088.294a.892.892 0 0 1 .763-.325c.281-.022.57.09.762.325l6.781 8.288a.893.893 0 0 1-1.381 1.13L7.85 2.182 1.688 9.713a.892.892 0 0 1-1.381-1.13z"/></g></g></svg></div>',
   });
 } else if (window.location.pathname == "/catalog.html") {
-
+  /*Ползунок цен */
+  jQuery(document).ready(function () {
+    $(".upper").on("input", setFill);
+    $(".lower").on("input", setFill);
+  
+    var max = $(".upper").attr("max");
+    var min = $(".lower").attr("min");
+  
+    function setFill(evt) {
+      var valUpper = $(".upper").val();
+      var valLower = $(".lower").val();
+      if (parseFloat(valLower) > parseFloat(valUpper)) {
+        var trade = valLower;
+        valLower = valUpper;
+        valUpper = trade;
+      }
+  
+      var width = (valUpper * 100) / max;
+      var left = (valLower * 100) / max;
+      $(".fill").css("left", "calc(" + left + "%)");
+      $(".fill").css("width", width - left + "%");
+  
+      // Update info
+      if (parseInt(valLower) == min) {
+        $(".easy-basket-lower").val("0");
+      } else {
+        $(".easy-basket-lower").val(parseInt(valLower));
+      }
+      if (parseInt(valUpper) == max) {
+        $(".easy-basket-upper").val("5000");
+      } else {
+        $(".easy-basket-upper").val(parseInt(valUpper));
+      }
+      $(".histogram-list li").removeClass("ui-histogram-active");
+    }
+  
+    // изменяем диапазон цен вручную
+    $(".easy-basket-filter-info p input").keyup(function () {
+      var valUpper = $(".easy-basket-upper").val();
+      var valLower = $(".easy-basket-lower").val();
+      var width = (valUpper * 100) / max;
+      var left = (valLower * 100) / max;
+      if (valUpper > 5000) {
+        var left = max;
+      }
+      if (valLower < 0) {
+        var left = min;
+      } else if (valLower > max) {
+        var left = min;
+      }
+      $(".fill").css("left", "calc(" + left + "%)");
+      $(".fill").css("width", width - left + "%");
+      // меняем положение ползунков
+      $(".lower").val(valLower);
+      $(".upper").val(valUpper);
+    });
+    $(".easy-basket-filter-info .iLower input").blur(function () {
+      var valLower = $(".lower").val();
+      $(this).val(Math.floor(valLower));
+    });
+    $(".easy-basket-filter-info .iUpper input").blur(function () {
+      var valUpper = $(".upper").val();
+      $(this).val(Math.floor(valUpper));
+    });
+  });
+  
 }
