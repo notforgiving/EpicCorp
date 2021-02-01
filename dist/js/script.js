c3e85bd5392d609bb8e7813650c6466368907132
notforgiving <notforgiving@list.ru>
2021-02-01 15:32:19 +0300

    correct sliderres

--- dist/js/script.js
+++ dist/js/script.js
@@ -132,7 +132,7 @@ if (window.location.pathname == "/") {
       '<div class="cutaway__recomendation-slider__arrows cutaway__recomendation-slider__arrows-right"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="10" viewBox="0 0 16 10"><g><g><path fill="#787677" d="M.307 8.582L7.088.294a.892.892 0 0 1 .763-.325c.281-.022.57.09.762.325l6.781 8.288a.893.893 0 0 1-1.381 1.13L7.85 2.182 1.688 9.713a.892.892 0 0 1-1.381-1.13z"/></g></g></svg></div>',
     responsive: [
       {
-        breakpoint: 375,
+        breakpoint: 376,
         settings: {
           dots: false,
           arrows: false,
